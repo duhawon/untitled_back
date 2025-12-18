e5279795475b3ed41ceea015975aeeb8ca102396
duhawon <duhawon1123@gmail.com>
2025-12-18 20:39:27 +0900

    first commit

---
+++ src/test/java/com/untitled/escape/EscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.untitled.escape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
